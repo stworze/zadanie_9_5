93188e2d45d582a737ebb0207c7472464422fc36
Sandra Banaszczyk <hejnowicz.s@gmail.com>
2018-01-10 01:20:56 +0100

    Add scripts

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var text = document.getElementsByClassName('button');
+document.getElementsByClassName('button').innerHTML = text.length;
+
+	alert(text[0].innerText);
+	alert(text[1].innerText);
+	alert(text[2].innerText);
