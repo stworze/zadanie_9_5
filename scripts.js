6f6f498f60b59d9ec8cfe1520c9cf559de483fb7
Sandra Banaszczyk <hejnowicz.s@gmail.com>
2018-01-17 22:23:38 +0100

    Change scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 var text = document.getElementsByClassName('button');
-document.getElementsByClassName('button').innerHTML = text.length;
 
-for (i = 0; i < text.length; i++) {
+for (var i = 0; i < text.length; i++) {
 alert(text[i].innerText);
 }
